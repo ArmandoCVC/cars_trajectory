b8c432f8f77b6620fe013bc0f459a50f1a8ead96
Cornelius1945 <covaci.armando22@gmail.com>
2024-05-15 10:55:13 +0200

    terminé

--- wp-content/themes/WordPress-theme-boilerplate-master/contact.php
+++ wp-content/themes/WordPress-theme-boilerplate-master/contact.php
@@ -3,9 +3,11 @@
 <?php get_header() ?>
 
 <section class="contact-section1">
+
     <div class="contact-section1__container">
         <?php the_content(); ?>
     </div>
+    
 </section>
 
 <?php get_footer() ?>
